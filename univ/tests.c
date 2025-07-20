db3a70a4d80bf80200b67b20403330596be73343
Matisse Rüdiger <matmatrpro@gmail.com>
2025-07-20 18:09:53 +0200

    Update code examples in tests.c

--- univ/tests.c
+++ univ/tests.c
@@ -25,6 +25,48 @@ int main(int argc, char **argv)
     }
 
 
+
+    // OPENGL
+
+
+    if (!SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3))
+    {
+        fprintf(stderr, "Failed at SDL_GL_SetAttribute major version setting: \n%s\n", SDL_GetError());
+        SDL_Quit();
+        return -1;
+    }
+
+    if (!SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3))
+    {
+        fprintf(stderr, "Failed at SDL_GL_SetAttribute minor version setting: \n%s\n", SDL_GetError());
+        SDL_Quit();
+        return -1;
+    }
+
+    if (!SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE))
+    {
+        fprintf(stderr, "Failed at SDL_GL_SetAttribute profile mask core: \n%s\n", SDL_GetError());
+        SDL_Quit();
+        return -1;
+    }
+
+    if (!SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1))
+    {
+        fprintf(stderr, "Failed at SDL_GL_SetAttribute double buffering: \n%s\n", SDL_GetError());
+        SDL_Quit();
+        return -1;
+    }
+
+    if (!SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24))
+    {
+        fprintf(stderr, "Failed at SDL_GL_SetAttribute depth size: \n%s\n", SDL_GetError());
+        SDL_Quit();
+        return -1;
+    }
+
+
+
+
     SDL_Window *mainWindow = SDL_CreateWindow("Main Test", 800, 600, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
     if (mainWindow == NULL)
     {
@@ -33,6 +75,10 @@ int main(int argc, char **argv)
         SDL_Quit();
         return -1;
     }
+
+
+    
+    // OPENGL
 
     SDL_GLContext mainWindowGL_ctx = SDL_GL_CreateContext(mainWindow);
     if (mainWindowGL_ctx == NULL)
@@ -43,6 +89,9 @@ int main(int argc, char **argv)
         return -1;
     }
 
+    gladLoadGLLoader(SDL_GL_GetProcAddress);
+
+    printf("Your OpenGL version: %s\n", glGetString(GL_VERSION));
 
 
 
@@ -51,7 +100,7 @@ int main(int argc, char **argv)
 
 
 
-    
+
 
     uint8_t run = 1;
     SDL_Event ev;

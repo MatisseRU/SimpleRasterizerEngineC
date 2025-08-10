9c55206ed2378f667705ae401062ccdd3638a82d
Matisse Rüdiger <matmatrpro@gmail.com>
2025-08-10 13:06:00 +0200

    Fixed makefile and added an option to add more CFLAGS dynamicaly, also added a simple 3D spining triangle to our collection of CGLM x OpenGL x SDL3 examples

--- makefile
+++ makefile
@@ -1,39 +1,25 @@
-# Nom de l'exécutable final
 TARGET = build/main.out
+TESTTARGET = build/tests.out
 
-# Fichiers source
 SRC = ./univ/main.c ./platforms/Linux/glad.c
 SRCTEST = ./univ/tests.c ./platforms/Linux/glad.c
 
-# Compilateur
-CC = gcc
-
-# Options de compilation
-CFLAGS = -Wall -Wextra -O2 `pkg-config --cflags sdl3`
-
-# Options d'édition de liens (SDL3 + OpenGL)
+CFLAGS = -Wall -Wextra -O2 `pkg-config --cflags sdl3` ${EXTRA_CFLAGS}
 LDFLAGS = `pkg-config --libs sdl3` -ldl -lm -lGL
 
-# Règle par défaut
 all: $(TARGET)
 
-# Règle de compilation
 $(TARGET): $(SRC)
 	@mkdir -p $(dir $@)
 	$(CC) $(CFLAGS) $(SRC) -o $(TARGET) $(LDFLAGS)
 
-# Compile le test / exemple
-example: $(TARGET)
-
-# Règle de compilation
-$(TARGET): $(SRCTEST)
+$(TESTTARGET): $(SRCTEST)
 	@mkdir -p $(dir $@)
-	$(CC) $(CFLAGS) $(SRCTEST) -o $(TARGET) $(LDFLAGS)
+	$(CC) $(CFLAGS) $(SRCTEST) -o $(TESTTARGET) $(LDFLAGS)
 
+example: $(TESTTARGET)
 
-
-# Nettoyage
 clean:
 	rm -rf build
 
-.PHONY: all clean
+.PHONY: all clean example test
--- platforms/Linux/linux.c
+++ platforms/Linux/linux.c
@@ -2,6 +2,7 @@
 #include "stb_image.h"
 #include <SDL3/SDL.h>
 #include "./glad.h"
+#include <cglm/cglm.h>
 #include <stdio.h>
 
 
@@ -84,6 +85,55 @@ unsigned int SRE_CreateDefaultCornerColoredShaderProgram(void)
     "{\n"
     "   vertexColor = aColor;\n"
     "   gl_Position = vec4(aPos, 1.0);\n"
+    "}\0";
+
+    const char* fragmentShaderSource_default =
+    "#version 330 core\n"
+    "in vec3 vertexColor;\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(vertexColor, 1.0);\n"
+    "}\n\0";
+
+
+    // OPENGL: create the Vertex Shader
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource_default, NULL);
+    glCompileShader(vertexShader);
+
+    // OPENGL: create the Fragment Shader
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource_default, NULL);
+    glCompileShader(fragmentShader);
+
+    // OPENGL: shader program
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    
+    // OPENGL: clean up things and return the shader program
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return shaderProgram;
+}
+unsigned int SRE_3D_CreateDefaultCornerColoredShaderProgram(void)
+{
+    const char* vertexShaderSource_default =
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "layout (location = 1) in vec3 aColor;\n"
+    "uniform mat4 uMVP;\n"
+    "out vec3 vertexColor;\n"
+    "void main()\n"
+    "{\n"
+    "   vertexColor = aColor;\n"
+    "   gl_Position = uMVP * vec4(aPos, 1.0);\n"
     "}\0";
 
     const char* fragmentShaderSource_default =

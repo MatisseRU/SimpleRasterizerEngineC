fe10787a1e6063bb23c37e90da396bb9c0631364
Matisse Rüdiger <matmatrpro@gmail.com>
2026-02-21 18:15:03 +0100

    Added function declaration for mesh manipulation at ring 2, also added a basic Mesh structure at ring 2

--- univ/ring2.h
+++ univ/ring2.h
@@ -15,6 +15,34 @@
 #define CAM_MOVE_DOWN 10
 
 
+
+/* Mesh manipulation (a mesh is a complete animated "character" or "pnj" or playable "character" composed of multiple Drawables) */
+
+typedef struct SRE_ring2_Mesh
+{
+    // local positions / rotations / scaling
+    // _
+    float xPos, yPos, zPos;
+    float xRotAngle, yRotAngle, zRotAngle;
+    float xScale, yScale, zScale;
+
+    void (*update)(struct SRE_ring2_Mesh *self);
+} SRE_ring2_Mesh;
+
+
+
+/* ring 2 methods */
+
+void _SRE_ring2_Mesh_do_Place_At(SRE_ring1_Model *self);
+
+
+
+/* Mesh Manipulation */
+
+void SRE_ring2_Make_Full_Mesh();
+
+
+
 /* Camera Movement Routines */
 void SRE_ring2_MoveCamera(const uint8_t cam_move_direction, float units);
 void SRE_ring2_RotateCamera(const uint8_t cam_rotation_direction, float units);

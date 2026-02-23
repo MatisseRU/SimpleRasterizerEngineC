67051b4a0b693ea0c496b8d99dad1b8d73865273
Matisse Rüdiger <matmatrpro@gmail.com>
2026-02-23 18:42:32 +0100

    Lowered down camera movement speed, also added sprint to camera rotations

--- univ/main.c
+++ univ/main.c
@@ -45,7 +45,7 @@ int main(int argc, char **argv)
     const bool *keys = SDL_GetKeyboardState(NULL);
 
     // amplifier (sprint camera)
-    float sprint = 1.0f;
+    float sprint = 2.0f;
 
     uint8_t run = 1;
     while (run)
@@ -71,55 +71,55 @@ int main(int argc, char **argv)
         // Forward movement (W)
         if (keys[SDL_SCANCODE_W])
         {
-            SRE_ring2_MoveCamera(CAM_MOVE_FORWARD, 0.05f * sprint);
+            SRE_ring2_MoveCamera(CAM_MOVE_FORWARD, 0.01f * sprint);
         }
 
         // Backward movement (S)
         if (keys[SDL_SCANCODE_S])
         {
-            SRE_ring2_MoveCamera(CAM_MOVE_BACKWARD, 0.05f * sprint);
+            SRE_ring2_MoveCamera(CAM_MOVE_BACKWARD, 0.01f * sprint);
         }
 
         // Left movement (A)
         if (keys[SDL_SCANCODE_A])
         {
-            SRE_ring2_MoveCamera(CAM_MOVE_LEFT, 0.05f * sprint);
+            SRE_ring2_MoveCamera(CAM_MOVE_LEFT, 0.01f * sprint);
         }
 
         // Right movement (D)
         if (keys[SDL_SCANCODE_D])
         {
-            SRE_ring2_MoveCamera(CAM_MOVE_RIGHT, 0.05f * sprint);
+            SRE_ring2_MoveCamera(CAM_MOVE_RIGHT, 0.01f * sprint);
         }
 
         // DOWN movement (LSHIFT)
         if (keys[SDL_SCANCODE_LSHIFT])
         {
-            SRE_ring2_MoveCamera(CAM_MOVE_DOWN, 0.05f * sprint);
+            SRE_ring2_MoveCamera(CAM_MOVE_DOWN, 0.01f * sprint);
         }
 
         // UP movement (SPACE)
         if (keys[SDL_SCANCODE_SPACE])
         {
-            SRE_ring2_MoveCamera(CAM_MOVE_UP, 0.05f * sprint);
+            SRE_ring2_MoveCamera(CAM_MOVE_UP, 0.01f * sprint);
         }
 
         // check camera rotations
         if (keys[SDL_SCANCODE_LEFT])
         {
-            SRE_ring2_RotateCamera(CAM_ROTATE_LEFT, 1.0f);
+            SRE_ring2_RotateCamera(CAM_ROTATE_LEFT, 0.25f * sprint);
         }
         if (keys[SDL_SCANCODE_RIGHT])
         {
-            SRE_ring2_RotateCamera(CAM_ROTATE_RIGHT, 1.0f);
+            SRE_ring2_RotateCamera(CAM_ROTATE_RIGHT, 0.25f * sprint);
         }
         if (keys[SDL_SCANCODE_UP])
         {
-            SRE_ring2_RotateCamera(CAM_ROTATE_UP, 1.0f);
+            SRE_ring2_RotateCamera(CAM_ROTATE_UP, 0.25f * sprint);
         }
         if (keys[SDL_SCANCODE_DOWN])
         {
-            SRE_ring2_RotateCamera(CAM_ROTATE_DOWN, 1.0f);
+            SRE_ring2_RotateCamera(CAM_ROTATE_DOWN, 0.25f * sprint);
         }
 
 
